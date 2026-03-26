72a9650b0e69856af56f79dbfed9d8b6a683c68a
Sanju Roy <sanjuroy806@gmail.com>
2026-03-27 00:35:49 +0530

    feat: enhance createProduct function with validation and error handling

--- Backend/src/controllers/product.controller.js
+++ Backend/src/controllers/product.controller.js
@@ -3,29 +3,50 @@ import { ApiError } from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import { Product } from "../models/product.model.js";
 
-const createProduct = asyancHandler(async()=>{
-const {title, description, price, category, image, stock} = req.body;
+const createProduct = asyancHandler(async (req, res) => {
+    const { name, description, price, category, stock } = req.body
+    if (!name == "") {
+        throw new ApiError(400, "Product name is required")
+    }
+    if (!description == "") {
+        throw new ApiError(400, "Product description is required")
+    }
 
-if(!title?.trim()){
-    throw new ApiError(400, "Title is Require");
-}
-if(!description?.trim()){
-throw new ApiError(400,"Description is Require")
+    if (price === undefined || price === null) {
+        throw new ApiError(400, "Product price is required")
+    }
+
+    if (!category == "") {
+        throw new ApiError(400, "Product category is required")
+    }
+
+    if (stock === undefined || stock === null) {
+        throw new ApiError(400, "Stock quantity is required")
+    }
+const product =  await Product.create({
+...req.body , 
+seller: req.user._id
+})
+
+if (!product) {
+    throw new ApiError(500, "Something went wrong while creating the product")
 }
 
-if(!price?.trim()){
-    throw new ApiError(400,"Price is Require")
-}
-if(!category?.trim()){
-    throw new ApiError(400,"Category Is Require")
-}
-
-if (!image?.trim()) {
-    throw new ApiError(400,"Image is Require")
-}
-if
+return res
+        .status(201)
+        .json(new ApiResponse(201, product, "Product created successfully"));
 
 
 })
+
+const getProduct = asyancHandler(async (req, res) => {
+    const product = await Product.find().sort({ createdAt: -1 })
+})
+
+
+const updateProduct = asyancHandler(async (req, res) => {
+
+})
+
 
 export { createProduct }
